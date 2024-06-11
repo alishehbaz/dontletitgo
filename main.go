d5569e09a03c62899a52304bf4c561f817c05444
Ali Shehbaz <alishahbaz072@gmail.com>
2024-06-10 21:25:05 -0400

    implement refill logic

--- main.go
+++ main.go
@@ -22,11 +22,14 @@ func newTokenBucket(capacity, refillFrequency int) *TokenBucket {
 }
 
 func (bucket *TokenBucket) canProcessRequests(numOfRequests int) bool {
+	bucket.refill()
 	fmt.Println(bucket.capacity)
 	fmt.Println(bucket.lastRefillStamp)
 
 	if numOfRequests <= bucket.numOfTokens {
 		bucket.numOfTokens -= numOfRequests
+		fmt.Println(numOfRequests, "have been consumed")
+		fmt.Println(bucket.numOfTokens, "tokens are remaning in the bucket")
 		return true
 	}
 
@@ -34,10 +37,33 @@ func (bucket *TokenBucket) canProcessRequests(numOfRequests int) bool {
 
 }
 
+func (bucket *TokenBucket) refill() {
+
+	now := time.Now()
+	elapsed := now.Sub(bucket.lastRefillStamp)
+	bucket.numOfTokens += int(elapsed.Seconds()) * bucket.refillFrequency
+	if bucket.numOfTokens > bucket.capacity {
+		bucket.numOfTokens = bucket.capacity
+	}
+	bucket.lastRefillStamp = time.Now()
+	fmt.Println("Time elapsed", elapsed)
+
+}
+
 func tokenBucket() {
 	bucket := newTokenBucket(10, 3)
 	fmt.Println("token bucket algorithm")
-	bucket.canProcessRequests(500)
+
+	// Make a call every 200ms aka 1/5th of a second
+
+	for i := 0; i < 15; i++ {
+		if bucket.canProcessRequests(2) {
+			fmt.Println("Token consumed")
+		} else {
+			fmt.Println("No token available")
+		}
+		time.Sleep(time.Millisecond * 200)
+	}
 
 }
 
