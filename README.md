567a22c62db1fdf4f7efed69f9c923a5ce52a797
Ali Shehbaz <alishahbaz072@gmail.com>
2024-06-10 21:48:55 -0400

    slight changes

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 #### I plan to implement all the main rate limiting algorithms in Go
 
 ##### Starting with Token Bucket
+
+To Run, make sure you have go installed and then type this in the terminal
+
+> go run main.go
+
+### TODOS
+
+TODO: Implement multithreading \
+TODO: Fix the issue with consuming the left over token, i.e when numOfTokens requested > current tokens left, so it ends up not using any token at all \
+TODO: Allow multiple time modes like seconds, minuts, hours, milliseconds
--- main.go
+++ main.go
@@ -23,8 +23,8 @@ func newTokenBucket(capacity, refillFrequency int) *TokenBucket {
 
 func (bucket *TokenBucket) canProcessRequests(numOfRequests int) bool {
 	bucket.refill()
-	fmt.Println(bucket.capacity)
-	fmt.Println(bucket.lastRefillStamp)
+	// fmt.Println(bucket.capacity)
+	// fmt.Println(bucket.lastRefillStamp)
 
 	if numOfRequests <= bucket.numOfTokens {
 		bucket.numOfTokens -= numOfRequests
@@ -42,27 +42,30 @@ func (bucket *TokenBucket) refill() {
 	now := time.Now()
 	elapsed := now.Sub(bucket.lastRefillStamp)
 	bucket.numOfTokens += int(elapsed.Seconds()) * bucket.refillFrequency
+	fmt.Println("Refilled! you have now", bucket.numOfTokens, "tokens")
 	if bucket.numOfTokens > bucket.capacity {
 		bucket.numOfTokens = bucket.capacity
 	}
-	bucket.lastRefillStamp = time.Now()
-	fmt.Println("Time elapsed", elapsed)
+	bucket.lastRefillStamp = now
+	// fmt.Println("Time elapsed", elapsed)
 
 }
 
 func tokenBucket() {
-	bucket := newTokenBucket(10, 3)
+	bucket := newTokenBucket(10, 2)
 	fmt.Println("token bucket algorithm")
 
-	// Make a call every 200ms aka 1/5th of a second
+	// Make a call every 1 second
+	// Make 45 calls in total
+	// Process 3 requests at once (trying to mimic traffic burst)
 
-	for i := 0; i < 15; i++ {
-		if bucket.canProcessRequests(2) {
+	for i := 0; i < 45; i++ {
+		if bucket.canProcessRequests(3) {
 			fmt.Println("Token consumed")
 		} else {
 			fmt.Println("No token available")
 		}
-		time.Sleep(time.Millisecond * 200)
+		time.Sleep(time.Second)
 	}
 
 }
