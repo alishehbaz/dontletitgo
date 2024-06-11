bb2dd109ebc6fff8215d81c146aabfa4538ee98a
Ali Shehbaz <alishahbaz072@gmail.com>
2024-06-10 20:51:07 -0400

    some progress

--- main.go
+++ main.go
@@ -2,23 +2,48 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 type TokenBucket struct {
-	capacity int
-	numOfTokens int
+	capacity        int
+	numOfTokens     int
+	refillFrequency int
+	lastRefillStamp time.Time
 }
 
-func tokenBucket(){
+func newTokenBucket(capacity, refillFrequency int) *TokenBucket {
+	return &TokenBucket{
+		capacity:        capacity,
+		numOfTokens:     capacity,
+		refillFrequency: refillFrequency,
+		lastRefillStamp: time.Now(),
+	}
+}
 
+func (bucket *TokenBucket) canProcessRequests(numOfRequests int) bool {
+	fmt.Println(bucket.capacity)
+	fmt.Println(bucket.lastRefillStamp)
+
+	if numOfRequests <= bucket.numOfTokens {
+		bucket.numOfTokens -= numOfRequests
+		return true
+	}
+
+	return false
+
+}
+
+func tokenBucket() {
+	bucket := newTokenBucket(10, 3)
 	fmt.Println("token bucket algorithm")
+	bucket.canProcessRequests(500)
 
-	
 }
 
 func main() {
 
 	fmt.Println("rate limit algorithms")
 	tokenBucket()
-	
+
 }
