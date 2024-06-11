70ea874913602d120d57f69838221538f4915e54
Ali Shehbaz <alishahbaz072@gmail.com>
2024-06-10 20:24:37 -0400

    init commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+type TokenBucket struct {
+	capacity int
+	numOfTokens int
+}
+
+func tokenBucket(){
+
+	fmt.Println("token bucket algorithm")
+
+	
+}
+
+func main() {
+
+	fmt.Println("rate limit algorithms")
+	tokenBucket()
+	
+}
